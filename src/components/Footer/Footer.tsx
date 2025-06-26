a0006a3c16918304b646fd5c3f986f4a3e47218e
Bruno Kemel <br.kemel@gmail.com>
2025-06-26 20:50:57 -0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import { Footer as StyledFooter } from "./styled"
 const Footer = () => {
   return (
     <StyledFooter>
-      <span>© {new Date().getFullYear()} Seu Nome. Todos os direitos reservados.</span>
-      <span>Desenvolvido com ❤️ usando React e Styled Components</span>
+      <span>© {new Date().getFullYear()} Bruno Kemel Corrêa Matos. Todos os direitos reservados.</span>
+      <span>Desenvolvido com ❤️ usando React+Vite e Styled Components</span>
     </StyledFooter>
   );
 }
--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -4,7 +4,7 @@ import { colors } from '../../styles/theme';
 export const Footer = styled.div`
   text-align: center;
   padding: 2rem;
-  color: ${colors.textLight};
+  color: ${colors.accent};
   font-size: 0.9rem;
   background: linear-gradient(135deg, ${colors.primary}05 0%, ${colors.secondary}05 100%);
 `;
