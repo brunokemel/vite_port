1f3a6e1e6b02a769fdbf6107573986509efde1eb
Bruno Kemel <br.kemel@gmail.com>
2025-08-31 16:43:40 -0300

    error -GMAIL- protuction

--- src/components/Contact/GMAIL/email.tsx
+++ src/components/Contact/GMAIL/email.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import emailjs from "emailjs-com";
+// import React, { useState } from "react";
+// import emailjs from "emailjs-com";
 
-export function contactEmail() {
-     const [message, setMessage] = useState("");
+// export function contactEmail() {
+//      const [message, setMessage] = useState("");
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+//   const handleSubmit = (e: React.FormEvent) => {
+//     e.preventDefault();
 
-    emailjs.send(
-      "seu_service_id",
-      "seu_template_id",
-      { message },
-      "sua_public_key"
-    )
-    .then(() => alert("Mensagem enviada com sucesso!"))
-    .catch(() => alert("Erro ao enviar mensagem"));
-  };
+//     emailjs.send(
+//       "seu_service_id",
+//       "seu_template_id",
+//       { message },
+//       "sua_public_key"
+//     )
+//     .then(() => alert("Mensagem enviada com sucesso!"))
+//     .catch(() => alert("Erro ao enviar mensagem"));
+//   };
 
-};
+// };
