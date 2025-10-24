ac5ec43ca1c2536a3ebc76049b55c9e68aa7b9ed
Bruno Kemel <br.kemel@gmail.com>
2025-10-24 17:56:10 -0300

    curriculo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -113,7 +113,7 @@ const Header: React.FC = () => {
           </SocialLinks>
 
           <DownloadButton
-            href="https://drive.google.com/drive/folders/1D6Mvrr6CJCBeudPF8u8zEO1EqRbGtgiI?usp=sharing"
+            href="../../../public/assets/curriculo.pdf"
             download
           >
             <DescriptionIcon />
